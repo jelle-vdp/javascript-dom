d5c6f6e84d0f377e6a3d3c82d2f6d5982caaa2be
jelle-vdp <jellevandepoel@gmail.com>
2022-05-06 15:57:44 +0200

    finished exercise seven

--- 07-generate-table-two-Optional/script.js
+++ 07-generate-table-two-Optional/script.js
@@ -11,6 +11,17 @@
 
 (function() {
 
-    // your code here
+    const target = document.getElementById("target");
+    let table = "<table><tbody>";
+    for (i = 1; i < 11; i++){
+        table = `${table}<tr>`;
+        for (j = 1; j < 11; j++){
+            table = `${table}<td>${i.toString()} x ${j.toString()} = ${i * j}</td>`;
+        };
+        table = `${table}</tr>`;
+    };
+    table = `${table}</tbody></table>`;
+
+    target.innerHTML = table;
 
 })();
